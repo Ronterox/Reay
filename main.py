86cb24a5f128b1338f67b5cdf2130dd743948102
Ricardo <ricarditomontserrat@gmail.com>
2025-03-30 23:45:28 +0200

    Finally full genetic functional training

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from concurrent.futures import ProcessPoolExecutor
 import random as rng
 import aigame
+import json
 
+from concurrent.futures import ProcessPoolExecutor
 from collections import defaultdict
 from enum import IntEnum
 
@@ -19,11 +20,13 @@ State = dict[str, Direction]
 
 
 class Game:
-    def __init__(self) -> None:
-        self.init = False
+    def __init__(self, epsilon: float = 1.0) -> None:
+        self.epsilon = epsilon
+        self.eps_decay = 0.995
+        self.eps_min = 0.01
 
-        self.player = None
-        self.score = 0
+        self.init = False
+        self.state: aigame.GameState
 
         self.mouse = aigame.Vector2(0, 0)
         self.actions = [Direction.UP, Direction.DOWN,
@@ -41,28 +44,24 @@ class Game:
 
     def step(self, action: Action) -> tuple[State, int, bool]:
         assert len(action) == 2 and action[0] in self.actions, 'Invalid action'
-        assert self.player is not None, 'Game not initialized with reset()'
+        assert self.state is not None, 'Game not initialized with reset()'
 
         sizew = aigame.SCREEN_WIDTH // aigame.CELLS_LIMIT
 
         direction = self.action_map[action[0]]
         self.mouse = aigame.Vector2(
-            (self.player.x + direction.x) * sizew,
-            (self.player.y + direction.y) * sizew,
+            (self.state.player_pos.x + direction.x) * sizew,
+            (self.state.player_pos.y + direction.y) * sizew,
         )
 
-        player = self.player
-        score = self.score
-
         game_state = aigame.update(self.mouse, action[1], False)
+        state, reward = self.env_model(self.state, game_state, action[0])
 
-        self.player = game_state.player_pos
-        self.score = game_state.score
+        self.state = game_state
 
-        state, reward = self.env_model(game_state, player, self.score > score, action[0])
         return state, reward, game_state.is_dead
 
-    def env_model(self, state: aigame.GameState, player: aigame.Vector2, safe: bool, action: Direction) -> tuple[State, int]:
+    def env_model(self, old_state: aigame.GameState, new_state: aigame.GameState, action: Direction) -> tuple[State, int]:
         assert action in self.actions, 'Invalid action'
         assert len(self.actions) == 5, 'Action space must be 5'
 
@@ -76,24 +75,38 @@ class Game:
         Or combinations for multiple chain of actions
         """
 
+        player = old_state.player_pos
+        safezone = old_state.safezone
+
         reward = 0
-        if state.is_dead:
-            reward = -10
-        elif safe:
-            reward = 1
+
+        old_distance = abs(player.x - safezone.x) + abs(player.y - safezone.y)
+        new_distance = abs(new_state.player_pos.x - new_state.safezone.x) + \
+            abs(new_state.player_pos.y - new_state.safezone.y)
+
+        if new_distance < old_distance:
+            reward += 2
+        elif new_distance > old_distance:
+            reward -= 1
+
+        if old_state.score < new_state.score:
+            reward += 5
+
+        if new_state.is_dead:
+            reward = -100
 
         key = ''
-        traps = [(t.x, t.y) for t in state.traps]
-        for action in self.actions:
-            direction = self.action_map[action]
-            key += f'{action.name[0]}'
+        traps = [(t.x, t.y) for t in old_state.traps]
+        for act in self.actions:
+            direction = self.action_map[act]
+            key += f'{act.name[0]}'
             if (player.x + direction.x, player.y + direction.y) in traps:
                 key += 'T '
             else:
                 key += 'F '
 
         key += 'W'
-        sz, px, py = state.safezone, player.x, player.y
+        sz, px, py = safezone, player.x, player.y
 
         if px == sz.x and py == sz.y:
             key += 'N'
@@ -116,10 +129,9 @@ class Game:
         game = aigame.update(self.mouse, False, True)
 
         self.mouse = aigame.Vector2(0, 0)
-        self.player = game.player_pos
-        self.score = 0
+        self.state = game
 
-        return self.env_model(game, self.player, False, self.actions[0])[0], False
+        return self.env_model(self.state, self.state, self.actions[0])[0], False
 
     def render(self) -> None:
         aigame.draw(self.mouse)
@@ -128,51 +140,64 @@ class Game:
         aigame.close()
 
 
-def none() -> tuple[Direction, int]:
-    return Direction.NONE, 0
+def none() -> tuple[Direction, float]:
+    return rng.choice([Direction.UP, Direction.DOWN,
+                       Direction.LEFT, Direction.RIGHT, Direction.NONE]), -float('inf')
 
 
-def train_agent(game: Game, best: defaultdict, episodes: int) -> tuple[defaultdict, float]:
-    epsilon = .8
-    eps_decay = 0.995
-    eps_min = 0.01
+def train_agent(game: Game, best: defaultdict, episodes: int, render: bool = False) -> tuple[defaultdict, float]:
     total_reward = 0
 
     for i in range(episodes):
         state, done = game.reset()
-        # print(f'Round {i+1}')
+        if render:
+            print(f'Round {i+1}')
 
         round_reward = 0
         frames = 0
-        while not done:
-            if frames % 4 == 0:
-                name = list(state.keys())[0]
 
-                if rng.random() < epsilon:
+        acc_state = State(default=Direction.NONE)
+        acc_reward = 0
+        acc_name = 'default'
+        while not done:
+            if frames % 20 == 0:
+                if render:
+                    print(acc_state[acc_name], acc_reward)
+
+                if rng.random() < game.epsilon:
                     state, reward, done = game.step(game.sample())
                 else:
-                    state, reward, done = game.step((best[name][0], True))
+                    state, reward, done = game.step((best[list(state.keys())[0]][0], True))
 
-                if reward > best[name][1]:
-                    best[name] = (state[name], reward)
-
-                round_reward += reward
-                epsilon = max(eps_min, epsilon * eps_decay)
+                acc_name = list(state.keys())[0]
+                acc_state = state
+                acc_reward = reward
+                game.epsilon = max(game.eps_min, game.epsilon * game.eps_decay)
             else:
                 state, reward, done = game.step((Direction.NONE, False))
+                acc_reward += reward
 
-            # game.render()
+                if acc_reward > best[acc_name][1]:
+                    best[acc_name] = (acc_state[acc_name], acc_reward)
+
+            round_reward += reward
+
+            if render:
+                game.render()
+
             frames += 1
 
         total_reward += round_reward
-        # print(f'Round reward: {round_reward}')
+        if render:
+            print(f'Round reward: {round_reward}')
 
     game.close()
 
     fitness = total_reward / episodes
-    # print('=' * 20)
-    # print(f'Moves learned: {len(best)}')
-    # print(f"Fitness: {fitness}")
+    if render:
+        print('=' * 20)
+        print(f'Moves learned: {len(best)}')
+        print(f"Fitness: {fitness}")
 
     return best, fitness
 
@@ -193,9 +218,9 @@ if __name__ == '__main__':
     elitism = 0.1
     mutation_rate = 0.2
 
-    agents = 30
+    agents = 50
     episodes = 100
-    generations = 5
+    generations = 10
 
     children = [defaultdict(none) for _ in range(agents)]
 
@@ -204,6 +229,8 @@ if __name__ == '__main__':
         print(f"Generation {i}...")
         results = deploy_agents((children * agents)[:agents], episodes)
         bests = sorted(results, key=lambda x: x[1], reverse=True)[:int(elitism * len(results))]
+
+        print(f'Overall Average: {sum([x[1] for x in results]) / len(results)}')
 
         children = []
         for (mom, mfit), (dad, dfit) in zip(bests[:-1], bests[1:]):
@@ -216,3 +243,9 @@ if __name__ == '__main__':
                 else:
                     child[key] = mom[key] if key in mom else dad[key]
             children.extend([mom, dad, child])
+
+    d, f = train_agent(Game(epsilon=0.), children[0], episodes, render=True)
+    print(d, f)
+
+    with open('best.json', 'w') as f:
+        json.dump(d, f)
